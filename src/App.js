5d1d3107f4e6a6830edd862128a231b98ad761dc
Annika Kaiser <ironannebonnie@gmail.com>
2018-07-21 11:57:03 +0200

    reworked basic scaffold

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from 'react';
-// import * as BooksAPI from './BooksAPI'
+// import * as BooksAPI from './BooksAPI';
 import './App.css';
-
+import AppContainer from './AppContainer';
+import BookSearch from './BookSearch';
 
 class BooksApp extends React.Component {
-  state = {
-      books: [],
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
 
   render() {
     return (
-        <div className="app">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-        </div>
+      <div className="app">
+        <AppContainer/>
+      </div>
     )
   }
 }
-
 export default BooksApp;
