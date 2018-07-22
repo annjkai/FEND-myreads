a51a76349928eb3870852dbfcd6075d3f2eb764b
Annika Kaiser <ironannebonnie@gmail.com>
2018-07-22 12:53:07 +0200

    state change without UI update

--- src/Book.js
+++ src/Book.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 
 class Book extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: this.props.currentShelf};
+        //console.log(this.state);
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
     render() {
-        //console.log(this.props.bookTitle);
+        console.log(this.props.currentShelf);
         return (
             <div className="book">
               <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.url}")` }}></div>
                 <div className="book-shelf-changer">
-                  <select
-                        /*onChange={(event) => this.props.changeShelf(
-                            this.props.book, event.target.value
-                        )}*/>
+                  <select value={this.state.value}  onChange={this.handleChange}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
